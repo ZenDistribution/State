f03e2ec425269f202b43e2dc95f6662d5f18135d
Ian Torres <iantorres@outlook.com>
2025-08-08 16:58:35 -0400

    Project initialized.

---
+++ include/zend/state/state.hpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2025 Ian Torres
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#ifndef ZEND_STATE_STATE_HPP
+#define ZEND_STATE_STATE_HPP
+
+#include <memory>
+
+namespace zend::state {
+    class state : public std::enable_shared_from_this<state> {
+        static constexpr char version_[] = "1.0.0";
+    public:
+        static const char * get_version() ;
+    };
+}
+
+#endif // ZEND_STATE_STATE_HPP
---
+++ tests/state_test.cc
@@ -0,0 +1,20 @@
+// Copyright (C) 2025 Ian Torres
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+#include <zend/state/state.hpp>
+
+TEST(state_test, get_version) {
+    EXPECT_STREQ(zend::state::state::get_version(), "1.0.0");
+}
